967ef44d9cb50e5822a2a01919d3517737e03752
Anthony081111 <ajh081111@gmail.com>
2021-10-16 11:35:44 -0400

    - Added clues surface.

--- game_board.py
+++ game_board.py
@@ -1,4 +1,5 @@
-""" This game is for up to 3 players.  You will need the locations on your for the animal pictures and player data """
+""" This game is for up to 3 players.  You will need the locations on your computer
+ for the animal pictures and player data """
 
 import pygame
 import os
@@ -10,45 +11,51 @@ def main():
     # players()
     # play(game)
 
+
 def initialize_game():
     """ Initialize game """
 
-    # Find on your computer the folder locations that will be used
-    animal_jpgs_path = r"C:\Users\sells\PycharmProjects\anthony_sat_oct_2\image"
-    player_data_path = r"C:\Users\sells\PycharmProjects\anthony_sat_oct_2\player_pictures"
+    # Find on your computer the folder locations that will be used.
+    # animal_jpgs_path = r"C:\Users\sells\PycharmProjects\anthony_sat_oct_2\image"
+    # player_data_path = r"C:\Users\sells\PycharmProjects\anthony_sat_oct_2\player_pictures"
+    # The next 2 URLs are for Anthony's computer
+    animal_jpgs_path = r"C:\imagee"
+    player_data_path = r"C:\Users\ajh08\PycharmProjects\anthony_sat_oct_two\player_pictures"
 
     # Set up game screen
     pygame.init()
     surface = game_surface()
-    player_surface = player_screen(3,player_data_path,surface)
+    player_surface = player_screen(3, player_data_path, surface)
     # text_surface()
-    # clue_surface()
+    clue_surface(animal_jpgs_path, r"Cairn-Terrier.jpg", surface)
     # rewards_surface()
     # input_surface()
     # game_over_surface()
 
 
 def game_surface():
-    surface = pygame.display.set_mode((1800, 1000))
+    surface = pygame.display.set_mode((1350, 700))
     pygame.display.set_caption("Anthony's Animal Wheel of Fortune")
     surface.fill((255, 0, 0))
     pygame.display.flip()           # this updates the entire screen
     return surface
 
-def player_screen(num_players,player_data_path,surface):
+
+def player_screen(num_players, player_data_path, surface):
     """ This function displays player information"""
-    os.chdir(player_data_path)       # Contains pictures of players and a player_file with previous scores and picture name
+    os.chdir(player_data_path)
+    # Contains pictures of players and a player_file with previous scores and picture name
 
     # display pictures of the players
     player1_image = pygame.image.load('anthony.jpg')
-    player1_image = pygame.transform.scale(player1_image, (400, 200))
-    surface.blit(player1_image, (50, 800))
+    player1_image = pygame.transform.scale(player1_image, (200, 200))
+    surface.blit(player1_image, (0, 500))
     player2_image = pygame.image.load('lisa.jpg')
-    player2_image = pygame.transform.scale(player2_image, (400, 200))
-    surface.blit(player2_image, (500, 800))
+    player2_image = pygame.transform.scale(player2_image, (200, 200))
+    surface.blit(player2_image, (200, 500))
     player3_image = pygame.image.load('michael.jpg')
-    player3_image = pygame.transform.scale(player3_image, (400, 200))
-    surface.blit(player3_image, (950, 800))
+    player3_image = pygame.transform.scale(player3_image, (200, 200))
+    surface.blit(player3_image, (400, 500))
     pygame.display.flip()
 
     # display current score.  At the beginning of game scores are 0.
@@ -66,20 +73,16 @@ def player_screen(num_players,player_data_path,surface):
             text_surface = font.render(name_score[row][column], False, (0, 255, 0))
             surface.blit(text_surface, ((x + 10) + (450 * column), (y + 10) + (row * 50)) )
             pygame.display.flip()
-    time.sleep(5)
     return surface
 
 
-
-
-
-
-
-
-
-
-
-
+def clue_surface(animal_jpgs_path, clue_file, surface):
+    os.chdir(animal_jpgs_path)
+    clue_image = pygame.image.load(clue_file)
+    clue_image = pygame.transform.scale(clue_image, (400, 400))
+    surface.blit(clue_image, (950, 300))
+    pygame.display.flip()
+    time.sleep(10)
 
 
 if __name__ == "__main__":
